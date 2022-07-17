fb2973c37b95d94644eb643d33bd87def04e2c41
Saugat Paudel <103073512+paudelsaugat02@users.noreply.github.com>
2022-07-17 22:30:41 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+$(".card1").on("mouseover", function(){
+    $(".card1").animate({
+        height: '8rem',
+        width: '18.5rem',
+    }, 300)
+})
+$(".card1").on("mouseleave", function(){
+    $(".card1").animate({
+        height: '7.7rem',
+        width: '18rem',
+    }, 180,);
+})
+
+
+$(".card2").on("mouseover", function(){
+    $(".card2").animate({
+        height: '8rem',
+        width: '18.5rem',
+    }, 300)
+})
+
+$(".card2").on("mouseleave", function(){
+    $(".card2").animate({
+        height: '7.7rem',
+        width: '18rem',
+    }, 180)
+})
+
+$(".card3").on("mouseover", function(){
+    $(".card3").animate({
+        height: '8rem',
+        width: '18.5rem',
+    }, 300)
+})
+
+$(".card3").on("mouseleave", function(){
+    $(".card3").animate({
+        height: '7.7rem',
+        width: '18rem',
+    }, 180)
+})
+
+$(".card4").on("mouseover", function(){
+    $(".card4").animate({
+        height: '8rem',
+        width: '18.5rem',
+    }, 300)
+})
+
+$(".card4").on("mouseleave", function(){
+    $(".card4").animate({
+        height: '7.7rem',
+        width: '18rem',
+    }, 180)
+})
